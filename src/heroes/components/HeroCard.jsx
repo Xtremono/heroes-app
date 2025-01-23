4bbe0d553e970bde4a1c4800084983501b6e7de0
jose tomas ruiz <j_tomas_93@hotmail.com>
2025-01-23 19:39:44 -0300

    Implementacion de heroes

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
 
+const CharactersByHero = ({alter_ego, characters}) => {
+  if (alter_ego === characters) return(<></>);
+  return <p>{characters}</p>
+}
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters,  }) => {
 
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
+  //const charactersByHero = (<p>{characters}</p>);
 
   return (
     <div className="col">
@@ -18,8 +24,15 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
                   <div className="card-body">
                     <h5 className="card-title">{superhero}</h5>
                     <p className="card-text">{alter_ego}</p>
+                    <CharactersByHero characters={ characters } alter_ego={alter_ego}/>
+                    <p className="card-text">
+                      <small className="text-muted">{first_appearance}</small>
+                    </p>
 
-                    <p>{characters}</p>
+                    <Link to={`/hero/${id}`}>
+                      Mas...
+                    </Link>
+
                   </div>
               </div>
 
